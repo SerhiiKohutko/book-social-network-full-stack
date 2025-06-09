9a45243c2067890e0483103f8dd1cafa1b72d865
Serhii Kohutko <serhiikohutko@gmail.com>
2025-06-09 14:04:23 +0300

    batman

---
+++ book-network-backend/src/main/java/org/example/booknetworkbackend/BookNetworkBackendApplication.java
@@ -0,0 +1,13 @@
+package org.example.booknetworkbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookNetworkBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BookNetworkBackendApplication.class, args);
+    }
+
+}
---
+++ book-network-backend/src/test/java/org/example/booknetworkbackend/BookNetworkBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.booknetworkbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookNetworkBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
