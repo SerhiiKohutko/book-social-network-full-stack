637eb75fb7f19d0398195a38358a40a2361040e0
Serhii Kohutko <serhiikohutko@gmail.com>
2025-06-11 12:59:43 +0300

    feat: repository add for role, user
    feat: token class create, add repository

---
+++ book-network-backend/src/main/java/org/example/booknetworkbackend/user/Token.java
@@ -0,0 +1,28 @@
+package org.example.booknetworkbackend.user;
+
+
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Getter
+@Setter
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class Token {
+
+    @Id
+    @GeneratedValue
+    private Long id;
+    private String token;
+    private LocalDateTime createdAt;
+    private LocalDateTime expiresAt;
+    private LocalDateTime validatedAt;
+
+    @ManyToOne
+    @JoinColumn(name = "userId", nullable = false)
+    private User user;
+}
